67d3ea042be4398fb7b724af8f925cb81f70d382
Gahyun Son <gahyunson@Gahyuns-MacBook-Pro.local>
2022-12-22 11:03:43 +0900

    day1 algorithm 문제

---
+++ day1.py
@@ -0,0 +1,9 @@
+def solution(n,a,b):
+    answer = 0
+
+    for i in range(1,n+1) :
+        if b%2==0 and b-a==1:
+            answer=i
+            return answer
+        a=(a//2+a%2)
+        b=(b//2+b%2)
