d86282726408aaaee69558ee69e44540906ff58a
gahyunson <chelsey.gahyun@gmail.com>
2024-04-30 23:36:03 +0900

    improve runtime 60ms to 46ms

--- Leetcode/DataStructure/LinkedList/addTwoNum.py
+++ Leetcode/DataStructure/LinkedList/addTwoNum.py
@@ -4,7 +4,24 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-class Solution:
+
+class Solution1:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dum = cur = ListNode(0)
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            cur.next = ListNode(carry%10)
+            carry = carry // 10
+            cur = cur.next
+        return dum.next
+    
+class Solution2:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         num1=""
         num2=""
@@ -16,7 +33,7 @@ class Solution:
             l2=l2.next
         num1, num2=num1[::-1], num2[::-1]        
         result = int(num1)+int(num2)
-
+        
         res_node = ListNode(0)
         nodes = res_node
         print(res_node)
