6f3ffbdd9def25a25bfdf49589f30a5d9e799623
GH <chelsey.gahyun@gmail.com>
2024-05-11 21:55:11 +0900

    another solution Sort List

--- Leetcode/DataStructure/LinkedList/sortList.py
+++ Leetcode/DataStructure/LinkedList/sortList.py
@@ -21,6 +21,42 @@ class Solution:
             newcur.next = ListNode(s)
             newcur = newcur.next
         return newhead.next
-            
+        
+        '''
+        if not head or not head.next:
+            return head
+
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next 
+            # fast = fast.next
+        mid = slow.next
+        slow.next = None
+
+        left = self.sortList(head) # 4 2
+        right = self.sortList(mid) # 1 3
+
+        dummy = ListNode(0)
+        curr = dummy
+        while left and right:
+            if left.val < right.val:
+                # print("if",left.val, right.val)
+                curr.next = left
+                left = left.next
+            else:
+                # print("else",left.val, right.val)
+                curr.next = right
+                right = right.next
+            # print("curr",curr)
+            curr = curr.next
+        # print("exit")
+        print('cr',curr)
+        print('left',left, 'right',right)
+        curr.next = left or right
+        print(curr)
+        # print("dummy",dummy)
+        return dummy.next
+        '''
 
             
