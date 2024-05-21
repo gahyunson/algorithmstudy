27cd97de9180e80bd0ce9afe55b386e3df30b6a0
GH <chelsey.gahyun@gmail.com>
2024-05-21 21:10:46 +0900

    add problem text

--- Python/exercise/fizzbuzz.py
+++ Python/exercise/fizzbuzz.py
@@ -1,3 +1,19 @@
+# Don't try be fancy.
+
+# --- Directions
+# Write a program that console logs the numbers
+# from 1 to n. But for multiples of three print
+# “fizz” instead of the number and for the multiples
+# of five print “buzz”. For numbers which are multiples
+# of both three and five print “fizzbuzz”.
+# --- Example
+#   fizzBuzz(5);
+#   1
+#   2
+#   fizz
+#   4
+#   buzz
+
 def fizzBuzz(n):
     for i in range(1,n+1):
         if i%3==0 and i%5==0:
