aa688230eb31ac956a3886150a1c7a1944f8c43f
GH <chelsey.gahyun@gmail.com>
2024-05-22 21:00:16 +0900

    solved chunk

---
+++ Python/exercise/chunk.py
@@ -0,0 +1,19 @@
+# --- Directions
+# Given an array and chunk size, divide the array into many subarrays
+# where each subarray is of length size
+# --- Examples
+# chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+# chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+# chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+# chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+# chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+def chunk(array, size):
+    result = []
+    for i in range(len(array)//size+1):
+        result.append(array[i*size:(i+1)*size])
+    print(result)
+    return result
+
+chunk([1,2,3,4,5,6,7,8,9],2)
+chunk([1,2,3,4,5],3)
