7cc87c664a9eac2fc08ee90903f649d543758498
GH <chelsey.gahyun@gmail.com>
2024-05-24 00:14:04 +0900

    another solution to anagrams

--- Python/exercise/anagrams.py
+++ Python/exercise/anagrams.py
@@ -9,14 +9,41 @@
 #   anagrams('Hi there', 'Bye there') --> False
 import re
 
+def anagrams3(stringA, stringB):    
+    reA = sorted(re.sub(r'\W', '', stringA).lower())
+    reB = sorted(re.sub(r'\W', '', stringB).lower())
+
+    if len(reA) != len(reB):
+        return False 
+    
+    dictA = {}
+    for a in reA:
+        if a not in dictA:
+            dictA[a] = 1
+        else:
+            dictA[a] += 1
+    dictB = {}
+    for b in reB:
+        if b not in dictB:
+            dictB[b] = 1
+        else:
+            dictB[b] += 1
+
+    for char in dictA:
+        if dictA[char] != dictB[char]:
+            return False
+    return True 
+
+
 def anagrams2(stringA, stringB):
     return cleanString(stringA) == cleanString(stringB)
 
 def cleanString(str):
-    reStr = re.sub(r'\W', '', str).lower().split().sort()
+    # in python, 'sorted' sorts 'string, tuple, list ... type'
+    reStr = sorted(re.sub(r'\W', '', str).lower())
     return reStr
 
-
+# print(cleanString('hello !ee'))
 
 def anagrams1(stringA, stringB):
     aCharMap = buildCharMap(stringA)
@@ -37,14 +64,20 @@ def buildCharMap(str):
             charMap[char] += 1
     return charMap
 
-print(anagrams1('hello', 'llohe'))
-print(anagrams1('Whoa! Hi!', 'Hi! Whoa!'))
-print(anagrams1('One One', 'Two two two'))
-print(anagrams1('One one', 'One one c'))
-print(anagrams1('A tree, a life, a bench', 'A tree, a fence, a yard'))
+# print(anagrams1('hello', 'llohe'))
+# print(anagrams1('Whoa! Hi!', 'Hi! Whoa!'))
+# print(anagrams1('One One', 'Two two two'))
+# print(anagrams1('One one', 'One one c'))
+# print(anagrams1('A tree, a life, a bench', 'A tree, a fence, a yard'))
 
-print(anagrams2('hello', 'llohe'))
-print(anagrams2('Whoa! Hi!', 'Hi! Whoa!'))
-print(anagrams2('One One', 'Two two two'))
-print(anagrams2('One one', 'One one c'))
-print(anagrams2('A tree, a life, a bench', 'A tree, a fence, a yard'))
+# print(anagrams2('hello', 'llohe'))
+# print(anagrams2('Whoa! Hi!', 'Hi! Whoa!'))
+# print(anagrams2('One One', 'Two two two'))
+# print(anagrams2('One one', 'One one c'))
+# print(anagrams2('A tree, a life, a bench', 'A tree, a fence, a yard'))
+
+print(anagrams3('hello', 'llohe'))
+print(anagrams3('Whoa! Hi!', 'Hi! Whoa!'))
+print(anagrams3('One One', 'Two two two'))
+print(anagrams3('One one', 'One one c'))
+print(anagrams3('A tree, a life, a bench', 'A tree, a fence, a yard'))
