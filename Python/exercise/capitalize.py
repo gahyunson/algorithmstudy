371a6290d07d636198d98018b1bfb26bbb22482c
GH <chelsey.gahyun@gmail.com>
2024-05-24 14:11:09 +0900

    solved capitalize

---
+++ Python/exercise/capitalize.py
@@ -0,0 +1,31 @@
+# --- Directions
+# Write a function that accepts a string.  The function should
+# capitalize the first letter of each word in the string then
+# return the capitalized string.
+# --- Examples
+#   capitalize('a short sentence') --> 'A Short Sentence'
+#   capitalize('a lazy fox') --> 'A Lazy Fox'
+#   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+def capitalize(str):
+    result = str[0].upper()
+
+    for i in range(1, len(str)):
+        if str[i-1] == ' ':
+            result = result + str[i].upper()
+        else:
+            result = result + str[i]
+    return result
+
+
+def capitalize1(str):
+    strArr = str.split()
+    result = []
+
+    for str in strArr:
+        result.append(str.replace(str[0], str[0].upper()))
+    return ' '.join(result)
+
+
+print(capitalize('hi there, how is it going?'))
+print(capitalize('i love breakfast at bill miller bbq'))
