9c73e967777b61fa745dcfc8f2540cb32abf2b4f
GH <chelsey.gahyun@gmail.com>
2024-05-31 20:45:38 +0900

    solved queue

---
+++ Python/datastructure.md
@@ -0,0 +1,18 @@
+*Let's make a queue structure without 'array'!*
+
+in javascript,  
+Below is adding and removing mechanism of a queue in javascript.
+- adding item in front of an array - > array.unshift()
+- removing something in the end of an array - > array.pop()
+
+Queue is ... good for complicated problem.
+
+Create a new, empty queue
+`const q = new Queue();`
+
+Add a record to a queue 
+`q.add(1);`
+
+Remove record at the end of a queue
+`q.remove();`   
+
---
+++ Python/queueStructure.py
@@ -0,0 +1,52 @@
+# --- Description
+# Create a queue data structure.  The queue
+# should be a class with methods 'add' and 'remove'.
+# Adding to the queue should store an element until
+# it is removed
+# --- Examples
+#     const q = new Queue();
+#     q.add(1);
+#     q.remove(); // returns 1;
+
+class Queue2:
+    def __init__(self) -> None:
+        self.queue = []
+    def add(self, n):
+        self.queue.insert(0, n)
+    def remove(self):
+        return self.queue.pop()
+    
+q2 = Queue2()
+q2.add(1)
+q2.add(2)
+q2.add(3)
+q2.remove()
+q2.add(4)
+q2.add(5)
+print(q2.queue)
+
+
+class Queue:
+    def __init__(self, queue = []):
+        self.queue = []
+
+    def add(self, queue, n):
+        queue = self.queue 
+
+        if len(queue)<1:
+            return queue.append(n)
+            
+        queue[1:], queue[0] = queue, n
+        return queue 
+    
+    def remove(self, queue):
+        return self.queue.pop()
+
+q = Queue()
+q.add(q, 1)
+q.add(q, 2)
+q.add(q, 3)
+q.remove(q)
+q.add(q, 4)
+q.add(q, 5)
+print(q.queue)
