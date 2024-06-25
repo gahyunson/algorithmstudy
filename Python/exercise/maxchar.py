c2d20f7d0b14b1accf97b6d7e15c77f014940eb6
GH <chelsey.gahyun@gmail.com>
2024-06-25 19:58:29 +0900

    add comment at maxchar

--- Python/exercise/maxchar.py
+++ Python/exercise/maxchar.py
@@ -5,22 +5,33 @@
 # maxChar("abcccccccd") === "c"
 # maxChar("apple 1231111") === "1"
 
+'''
+I first reated a string dictionary. Then, I searched for the key corresponding to the maximum value in the dictionary.
+'''
+
 def maxChar1(str):
+    # empty object
     dic = {}
 
     for s in str:
+        # I received every character
+        # I'll add an entry to dic.
         if s not in dic:
         # if not dic[s]: -> KeyError
             dic[s]=1
+        # if en entry already exists, just add one to the number.
         else:
             dic[s]+=1
     
-    max_num = 0
-    char = ''
-    for k in dic:
-        if dic[k]>max_num:
-            max_num = dic[k]
-            char = k 
+    # find the character that was used most frequently in a given string.
+    max_num = 0 # max set to zero 
+    char = '' # char set to an empty string
+    for k in dic: 
+        # k is assigned the keys inside dic object
+        if dic[k]>max_num: # if we ever find a character that has more uses than Max, then we'll set Max equal to that new value
+            max_num = dic[k] # max_num become dic at k
+            char = k # I'll set char to the character that was responsible for that number of uses.
+            # char will become k
     return char 
 
 
