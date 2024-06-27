4d3ea4fe43afabe762006f6b74aa9587f0337549
GH <chelsey.gahyun@gmail.com>
2024-06-27 21:17:20 +0900

    add comment fizz buzz

--- Python/exercise/fizzbuzz.py
+++ Python/exercise/fizzbuzz.py
@@ -16,12 +16,18 @@
 
 def fizzBuzz(n):
     for i in range(1,n+1):
+        # Is the number a multiple of 3 and 5?
         if i%3==0 and i%5==0:
             print('fizzbuzz')
+            # and I don't want to excute any other code inside the for loop.
+        # Is the number a multiple of 5?
         elif i%5==0:
             print('buzz')
+        # Is the number a multiple of 3?
         elif i%3==0:
             print('fizz')
+        # if it's not a multiple of 15, 5 and 3,
+        # just print out the number
         else:
             print(i)
 
