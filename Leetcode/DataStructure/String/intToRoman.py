f945a94be2cf19cb18f645691b496fac72948f80
GH <chelsey.gahyun@gmail.com>
2024-08-07 00:37:46 +0900

    solving int to roman

--- Leetcode/DataStructure/String/intToRoman.py
+++ Leetcode/DataStructure/String/intToRoman.py
@@ -21,3 +21,31 @@ def intToRoman(self, num: int) -> str:
 		num //= 10
 
 	return "".join(ans[::-1])
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values = [1000, 500, 100, 50, 10, 5, 1]
+        symbols = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
+
+        result = ""
+        i = 0
+        while i != len(values):
+            cnt = num // values[i]
+            print(num, '/', values[i], '=', cnt, '...', num%values[i], i)
+            if num >= values[i]:
+                result += symbols[i] * (cnt)
+                num = num%values[i]
+            else:
+                if num // values[i+1] == 4:
+                    result = result + symbols[i+1] + symbols[i]
+                    num = num%values[i+1]
+                elif num // values[i+2] == 9:
+                    result = result + symbols[i+2] + symbols[i]
+                    num = num%values[i+2]
+            if num == 0:
+                return result
+            i += 1
+                
+
+
+
