ac3a04a64b4c6c45b78af348826d5e11b16e5bd7
GH <chelsey.gahyun@gmail.com>
2024-08-20 22:38:05 +0900

    add comment on jump game2

--- Leetcode/DataStructure/String/jumpGame2.py
+++ Leetcode/DataStructure/String/jumpGame2.py
@@ -12,6 +12,58 @@
 # Output: 2
 # Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
 
+'''
+Example: [2,3,1,1,4]
+
+We can transform the array [2,3,1,1,4] to [2,4,3,4,8].   
+
+It can 2 steps from 0 index.
+It can 4 steps from 0 index.
+It can 3 steps from 0 index.
+It can 4 steps from 0 index.
+
+what you can observe with [2,4,3,4,8] is that   
+you can evaluate each element without having to count each index individually.
+--you can check the element without counting each index.--
+
+Check all elements from index 0 to the last index.
+(You might be able to return before reaching the end! If it determined that the last index is reachable)
+
+1. The greater value can reach the last index.
+farthest: 2 -> 3 
+
+2. `farthest >= len(nums)-1`
+If farthest == 3 , then len(nums)-1 = 4 
+    3 >= 4 => It reached!
+
+3. `i==end`
+The point is each range.
+You define the maximum number within the range from 0 to end as farthest.   
+You will select the number within the range, you can count it as 1 jump.
+Next, You will reach the last index of the array or select again the max number then, end define the max number plus 1 to jump.
+
+0~end 범위 안에서 가장 큰 값을 다음 end로 지정하기 위해 farthest 를 정의한다. 
+아무튼 end 범위 안에서 하나의 숫자를 선택했으므로 1 jump를 한 것으로 간주하고,
+그 다음 end 범위 안에서 마지막 인덱스에 도착하던가 아니면 또 다시 가장 큰값을 고르고 그 다음 end값으로 지정하여 2 jump 한 것으로 계산된다.
+
+'''
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        cnt = 0
+        end = 0
+        farthest = 0
+
+        for i in range(len(nums) - 1):
+            farthest = max(farthest, i+nums[i])
+            if farthest >= len(nums)-1:
+                cnt += 1
+                return cnt
+            if i==end:
+                cnt += 1
+                end = farthest
+        return cnt
+
 class Solution:
     def jump(self, nums: List[int]) -> int:
         cnt = 0
