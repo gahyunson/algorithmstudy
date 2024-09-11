ed859fd4f77dbfb66df708ae15f2509b31775098
GH <chelsey.gahyun@gmail.com>
2024-09-11 21:55:43 +0900

    add comments on is subsequence

--- Leetcode/DataStructure/String/isSubsequence.py
+++ Leetcode/DataStructure/String/isSubsequence.py
@@ -1,6 +1,34 @@
 # 392. Is Subsequence
 
+'''
+I used two pointers.
+One for stirng s, 무ㅇ the other one for stirng t.
+'''
+
 class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        # if s is a substring of t, you can directly return True.
+        if s in t:
+            return True
+        
+        # you can scan the elements of both strings 's' and 't'.
+        # If s[si] and t[ti] are same, it means that the current character of 's' is found in 't'.
+        si = 0
+        ti = 0
+        while ti < len(t) and si < len(s):
+            if s[si] == t[ti]:
+                si += 1
+            ti += 1
+        
+        # If you've gone through all characters in 's', return True
+        if si == len(s):
+            return True
+        return False
+        
+        
+
+
+class Solution2:
     def isSubsequence(self, s: str, t: str) -> bool:
 
         if not s:
