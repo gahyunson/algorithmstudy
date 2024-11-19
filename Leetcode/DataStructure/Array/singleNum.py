3cb78bcdfafb4a8426bed440fba85770dd30a86e
GH <chelsey.gahyun@gmail.com>
2024-11-19 22:10:05 +0900

    136

---
+++ Leetcode/DataStructure/Array/singleNum.py
@@ -0,0 +1,12 @@
+# 136. Single Number
+
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        n_cnt = Counter(nums)
+        for n in n_cnt:
+            if n_cnt[n]==1:
+                return n
